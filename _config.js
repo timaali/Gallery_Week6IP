aaf58f23d063722b59569bcf9bbb0d686b0026c0
tima ali <timaali43@gmail.com>
2023-03-21 08:32:00 +0300

    Added .env file, node modules file and database URLs

---
+++ _config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://timaali:Tifaly@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://timaali:Tifaly@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://timaali:Tifaly@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+}
+module.exports = config;
