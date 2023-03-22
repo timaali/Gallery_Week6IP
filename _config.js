def149ab8568d2fb6131990bfffc1c6113736a0e
tima ali <timaali43@gmail.com>
2023-03-22 12:53:39 +0300

    Corrected the database URL

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://timaali:Tifaly@week6-ip.mumsbog.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://timaali:Tifaly@week6-ip.mumsbog.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://timaali:Tifaly@week6-ip.mumsbog.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://GalleryApp:galleryapp@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://GalleryApp:galleryapp@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://GalleryApp:galleryapp@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
 
