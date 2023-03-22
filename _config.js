6640adc89e4318029c0f038f770a07b97d62a813
tima ali <timaali43@gmail.com>
2023-03-22 12:22:10 +0300

    Corrected the database URL

--- _config.js
+++ _config.js
@@ -2,8 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://timaali:Tifaly@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://timaali:Tifaly@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://timaali:Tifaly@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://timaali:Tifaly@week6-ip.mumsbog.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://timaali:Tifaly@week6-ip.mumsbog.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://timaali:Tifaly@week6-ip.mumsbog.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
+
